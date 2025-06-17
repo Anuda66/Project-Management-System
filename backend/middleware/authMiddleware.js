9229a220dd970d5e2c91a41ccf49e2a6e17d255c
Anuda Ransara <anudaransara1@gmail.com>
2025-06-17 23:15:32 +0530

    Middeleware for Admin-only access

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,7 +5,7 @@ const User = require('../models/userModel');
 const protect = async (req, res, next) => {
     try {
         let token = req.headers.authorization;
-        if (!token || !token.startsWith('Bearer')) {
+        if (token && token.startsWith('Bearer')) {
             token = token.split(' ')[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.id).select('-password');
@@ -19,3 +19,15 @@ const protect = async (req, res, next) => {
         res.status(401).json({ message: 'Not authorized, token failed' });
     }
 }
+
+//Middeleware for Admin-only access-------------------
+const adminOnly = (req, res, next) => {
+    if( req.user && req.user.role === 'admin') {
+        next();
+    }
+    else {
+        res.status(403).json({ message: 'Access denied, admin only' });
+    }
+}
+
+module.exports = { protect, adminOnly };
